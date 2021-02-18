0064a6de5f2ca047eed4ca7e8472322289c2a33f
Grumpier <smanus@me.com>
2021-02-18 14:45:35 +0100

    Made all fonts System

--- MyRecipe/Model/RecipeBrain.swift
+++ MyRecipe/Model/RecipeBrain.swift
@@ -8,37 +8,24 @@
 import Foundation
 
 struct RecipeBrain {
-    static let recipeURL = URL(fileURLWithPath: "Recipes", relativeTo: FileManager.documentDirectoryURL)
-    static let ingredientURL = URL(fileURLWithPath: "Ingredients", relativeTo: FileManager.documentDirectoryURL)
     var ingredients: [Ingredient]
     var recipeList: [Recipe]
-    
-    init() {
-        do {
-            let ingredientData = try Data(contentsOf: RecipeBrain.ingredientURL)
-            self.ingredients = Array(ingredientData)
-        } catch {
-            print("Some kind of error retrieving ingredients")
-        }
-
-        let savedFavoriteBytes = Array(savedFavoriteBytesData)
-
-        
-    }
-    
     
     static func setIngredient(ingredient: Ingredient) {
         
         
     }
     
-    func writeRecipes {
+    func writeRecipes() {
         
         
     }
     
     
-    func readRecipes
+    func readRecipes(){
+        
+        
+    }
     
     
 }
